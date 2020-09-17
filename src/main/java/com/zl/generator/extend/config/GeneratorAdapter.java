7115aef98b1238ea6c05348e6f8ef1ce321308c3
lei.zhu <lei.zhu@yelopack.com>
2020-09-17 16:19:03 +0800

    2

--- src/main/java/com/zl/generator/extend/config/GeneratorAdapter.java
+++ src/main/java/com/zl/generator/extend/config/GeneratorAdapter.java
@@ -10,11 +10,8 @@ import org.mybatis.generator.api.dom.xml.Attribute;
 import org.mybatis.generator.api.dom.xml.Document;
 import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
-import org.mybatis.generator.codegen.mybatis3.ListUtilities;
 import org.mybatis.generator.codegen.mybatis3.MyBatis3FormattingUtilities;
-import org.springframework.beans.factory.annotation.Value;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -26,7 +23,6 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 public class GeneratorAdapter extends PluginAdapter{
-    private final String deleteByPrimaryKey = "deleteByPrimaryKey";
     private final String insert = "insert";
     private final String insertSelective = "insertSelective";
     private final String selectByPrimaryKey = "selectByPrimaryKey";
